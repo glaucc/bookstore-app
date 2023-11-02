86a4d8e618e27fe545ce8db6a5e1910c6d7eea08
glaucc <ahmedovyusif8785@gmail.com>
2023-11-02 17:05:34 +0400

    partially working home page and fully working login page

--- app/(main)/home/page.tsx
+++ app/(main)/home/page.tsx
@@ -4,28 +4,18 @@ import { useState, useEffect } from 'react';
 import styles from '../../../styles/Home.module.css';
 import { Folder } from 'lucide-react';
 
-const dashboardData = [
-  { entry: { isFile: true, name: "Sample Book 1" } },
-  { entry: { isFile: true, name: "Sample Book 2" } },
-  { entry: { isFile: false, name: "Sample Book 3" } },
-  { entry: { isFile: false, name: "Sample Book 3" } },
-  { entry: { isFile: false, name: "Sample Book 3" } },
-  { entry: { isFile: false, name: "Sample Book 3" } },
-  // Add more data as needed
-];
-
 const HomePage = () => {
   const [displayName, setDisplayName] = useState('');
-  // const [files, setFiles] = useState([]); for real API
-  const [files, setFiles] = useState(dashboardData);
+  const [files, setFiles] = useState<any[]>([]); // Ensure proper initialization here
 
-  
+  const authHeaders = {
+    'Cookie': 'ALFRESCO_REMEMBER_ME=1',
+    'Authorization': 'Basic cmVhY3Q6MTIzNDU2', // Add your Base64 encoded credentials here
+  };
 
   useEffect(() => {
     fetch('https://1curd3ms.trials.alfresco.com/alfresco/api/-default-/public/alfresco/versions/1/people/-me-', {
-      headers: {
-        'Cookie': 'ALFRESCO_REMEMBER_ME=1',
-      },
+      headers: authHeaders,
     })
       .then((response) => response.json())
       .then((data) => {
@@ -38,9 +28,7 @@ const HomePage = () => {
     fetch(
       'https://1curd3ms.trials.alfresco.com/alfresco/api/-default-/public/alfresco/versions/1/nodes/382b3102-ffba-422e-8711-d7f330fb5468/children?maxItems=25&orderBy=isFolder%20desc%2Cname%20ASC&include=path%2Cproperties%2CallowableOperations%2Cpermissions%2CaspectNames%2CisFavorite%2Cdefinition&includeSource=true',
       {
-        headers: {
-          'Cookie': 'ALFRESCO_REMEMBER_ME=1',
-        },
+        headers: authHeaders,
       }
     )
       .then((response) => response.json())
@@ -56,38 +44,37 @@ const HomePage = () => {
 
   return (
     <div className={styles.body}>
-
-    <div className={styles.container}>
-      <header className={styles.header}>
-        <h1 className={styles.logo}>Bookworm</h1>
-        <nav className={styles.navbar}>
-          <ul className={styles.navbarList}>
-            <li>Home</li>
-            <li>Books</li>
-            <li>Authors</li>
-            <li>Categories</li>
-            <li>{displayName || 'My Account'}</li>
-          </ul>
-        </nav>
-      </header>
-      <div className={styles.dashboard}>
-        <h2>Dashboard</h2>
-        <div className={styles.horizontalDashboard}>
-          {files.map((file, index) => (
-            <div key={index} className={styles.bookContainer}>
-              <div className={styles.book}>
-                {file.entry.isFile ? <Folder className={styles.folderIcon} /> : ""}
-                <p>{file.entry.name}</p>
+      <div className={styles.container}>
+        <header className={styles.header}>
+          <h1 className={styles.logo}>Bookworm</h1>
+          <nav className={styles.navbar}>
+            <ul className={styles.navbarList}>
+              <li>Home</li>
+              <li>Books</li>
+              <li>Authors</li>
+              <li>Categories</li>
+              <li>{displayName || 'My Account'}</li>
+            </ul>
+          </nav>
+        </header>
+        <div className={styles.dashboard}>
+          <h2>Dashboard</h2>
+          <div className={styles.horizontalDashboard}>
+            {files.map((file, index) => (
+              <div key={index} className={styles.bookContainer}>
+                <div className={styles.book}>
+                  {file.entry && !file.entry.isFile ? <Folder className={styles.folderIcon} /> : ''}
+                  <p>{file.entry && file.entry.name}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
+        <footer className={styles.footer}>
+          <p>© 2023 Bookworm. All Rights Reserved.</p>
+        </footer>
       </div>
-      <footer className={styles.footer}>
-        <p>© 2023 Bookworm. All Rights Reserved.</p>
-      </footer>
     </div>
-</div>
   );
 };
 
--- components/login.tsx
+++ components/login.tsx
@@ -1,46 +1,67 @@
 import { useState } from "react";
-import styles from '../styles/AuthenticationPage.module.css';
+import styles from "../styles/AuthenticationPage.module.css";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 const AuthenticationPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [isTrue, setIsTrue] = useState(false);
   const router = useRouter();
 
-  const handleSubmit = (event:any) => {
-    event.preventDefault();
-    if (username === "react" && password === "123456") {
-      router.push('/home');
-    } else {
-      alert("Incorrect username or password. Please try again.");
-    }
+  const encodeToBase64 = (str) => {
+    return btoa(str);
   };
 
-  // For real API data
-  // const handleSubmit = async (event:any) => {
-  //   event.preventDefault();
-  //   const url = 'https://1curd3ms.trials.alfresco.com/alfresco/api/-default-/public/authentication/versions/1/tickets';
-  //   const requestOptions = {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       'Cookie': 'ALFRESCO_REMEMBER_ME=1', // Add the cookie here
-  //     },
-  //     body: JSON.stringify({
-  //       userId: username,
-  //       password: password,
-  //     }),
-  //   };
-  //   try {
-  //     const response = await fetch(url, requestOptions);
-  //     const data = await response.json();
-  //     console.log(data); // Handle the response data here
-  //   } catch (error) {
-  //     console.error('Error:', error);
-  //   }
-  // };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // For real API data
+    const url =
+      "https://1curd3ms.trials.alfresco.com/alfresco/api/-default-/public/authentication/versions/1/tickets";
+    const authString = `${username}:${password}`;
+    const encodedAuthString = encodeToBase64(authString);
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Basic ${encodedAuthString}`, // Add the Authorization header
+      },
+      body: JSON.stringify({
+        userId: username,
+        password: password,
+      }),
+    };
+    try {
+      const response = await fetch(url, requestOptions);
+      const data = await response.json();
+      console.log(data); // Log the response data
+
+      // Check if the response data contains the expected "id" entry
+      if (data.entry && data.entry.id) {
+        document.cookie = `ALFRESCO_REMEMBER_ME=1`; // Save the cookie
+
+        // Make subsequent GET request with the same authentication
+        const getOptions = {
+          method: "GET",
+          headers: {
+            Authorization: `Basic ${encodedAuthString}`,
+          },
+        };
+        const response = await fetch(
+          'https://1curd3ms.trials.alfresco.com/alfresco/api/-default-/public/alfresco/versions/1/nodes/382b3102-ffba-422e-8711-d7f330fb5468/children?maxItems=25&orderBy=isFolder%20desc%2Cname%20ASC&include=path%2Cproperties%2CallowableOperations%2Cpermissions%2CaspectNames%2CisFavorite%2Cdefinition&includeSource=true',
+          getOptions
+        );
+        const data = await response.json();
+        console.log(data); // Log the response data for the GET request
+
+        router.push("/home"); // Navigate to the home page
+      } else {
+        alert("Incorrect username or password. Please try again.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Failed to authenticate. Please try again later.");
+    }
+  };
 
   return (
     <div className={styles.container}>
@@ -71,7 +92,6 @@ const AuthenticationPage = () => {
       </div>
     </div>
   );
-
 };
 
 export default AuthenticationPage;
